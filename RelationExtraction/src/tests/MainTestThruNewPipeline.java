fcfb283669d5573b5b722ab863493c1c19f7e1a8
jajajaqlt <724272859@qq.com>
2015-10-04 18:13:19 -0500

    Gets ready for writing wrapper.

--- RelationExtraction/src/tests/MainTestThruNewPipeline.java
+++ RelationExtraction/src/tests/MainTestThruNewPipeline.java
@@ -5,7 +5,7 @@ import pipeline.NewPipeline;
 public class MainTestThruNewPipeline {
 	public static void main(String[] args) throws Exception {
 		NewPipeline.run("NETMETA", "SRSTR", "SRDEF", "COMPACT_MRREL.RRF",
-				"input.txt", "output.txt", "errorLog.txt", "wordDict.txt",
+				"input_test.txt", "output_ex_151004_test2.txt", "errorLog.txt", "wordDict.txt",
 				"tagDict.txt", "depDict.txt", false);
 	}
 }
