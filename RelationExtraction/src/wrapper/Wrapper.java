617107e5475d5c4f8b9b94ba74ca00ace3b66f78
jajajaqlt <724272859@qq.com>
2015-10-09 17:23:08 -0500

    Correctly reads a pipeline file into an Instance object.

--- RelationExtraction/src/wrapper/Wrapper.java
+++ RelationExtraction/src/wrapper/Wrapper.java
@@ -7,22 +7,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import edu.stanford.nlp.util.HashIndex;
+import edu.stanford.nlp.util.Pair;
 
 public class Wrapper {
 
-	public static String inputFile = "wrapper_input.txt";
+	public static String inputFile = "output_ex_151007_test.txt";
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// loads file into a buffered reader
 		BufferedReader br = new BufferedReader(new FileReader(inputFile));
 		String line;
-		String[] tokens;
 		// separates name and value in the basic info of a instance
-		String separator = ": ";
+		int sLvFeatsCount, chunksCount, phrasesCount, wordsCount;
 		Instance instance;
 		HashIndex<String> sLvConjIndex = new HashIndex<String>();
-		int index;
-		ArrayList<Integer> featIndices;
+		HashIndex<String> cLvConjIndex = new HashIndex<String>();
+		HashIndex<String> pLvConjIndex = new HashIndex<String>();
+		HashIndex<String> wLvConjIndex = new HashIndex<String>();
+		// ArrayList<Integer> featIndices;
+		Pair<Integer, Integer> pair;
 
 		// boolean insideInstance = false;
 		try {
@@ -30,103 +33,41 @@ public class Wrapper {
 				// if (!insideInstance) {
 				if (line.substring(0, 10).equals("<instance>")) {
 					try {
-						// insideInstance = true;
-						instance = new Instance();
-						// index
-						tokens = br.readLine().split(separator);
-						instance.index = Integer.parseInt(tokens[1]);
+						// header
+						instance = readBasicInfoAndInitializeInstance(br);
 
-						// cui1
-						tokens = br.readLine().split(separator);
-						instance.cui1 = tokens[1];
-						// cui1-type
-						tokens = br.readLine().split(separator);
-						instance.cui1_type = tokens[1];
-						// cui1_matched_words
-						tokens = br.readLine().split(separator);
-						instance.cui1_matched_words = tokens[1];
-						// cui1_index
-						tokens = br.readLine().split(separator);
-						instance.cui1_index = Integer.parseInt(tokens[1]);
+						// sentence-level features
+						readSentenceLevelFeatures(br, instance, sLvConjIndex);
 
-						// cui2
-						tokens = br.readLine().split(separator);
-						instance.cui2 = tokens[1];
-						// cui2-type
-						tokens = br.readLine().split(separator);
-						instance.cui2_type = tokens[1];
-						// cui2_matched_words
-						tokens = br.readLine().split(separator);
-						instance.cui2_matched_words = tokens[1];
-						// cui2_index
-						tokens = br.readLine().split(separator);
-						instance.cui2_index = Integer.parseInt(tokens[1]);
+						// chunks
+						pair = readFeatureBlock(br, cLvConjIndex,
+								"<chunk-level-features>", instance.chunksCnt,
+								instance, instance.cLvConjFeats,
+								instance.cLvBOWFeats);
+						instance.cLvNEIndex1 = pair.first;
+						instance.cLvNEIndex2 = pair.second;
 
-						// positivity
-						tokens = br.readLine().split(separator);
-						instance.positivity = Boolean.parseBoolean(tokens[1]);
-						// inverse
-						tokens = br.readLine().split(separator);
-						instance.inverse = Boolean.parseBoolean(tokens[1]);
+						// phrases
+						pair = readFeatureBlock(br, pLvConjIndex,
+								"<phrase-level-features>", instance.phrasesCnt,
+								instance, instance.pLvConjFeats,
+								instance.pLvBOWFeats);
+						instance.pLvNEIndex1 = pair.first;
+						instance.pLvNEIndex2 = pair.second;
 
-						// net-relation
-						tokens = br.readLine().split(separator);
-						instance.net_relation = tokens[1];
-						// meta-relation
-						tokens = br.readLine().split(separator);
-						instance.meta_relation = tokens[1];
+						// words
+						pair = readFeatureBlock(br, wLvConjIndex,
+								"<word-level-features>", instance.wordsCnt,
+								instance, instance.wLvConjFeats,
+								instance.wLvBOWFeats);
+						instance.wLvNEIndex1 = pair.first;
+						instance.wLvNEIndex2 = pair.second;
 
-						// sentence
-						tokens = br.readLine().split(separator);
-						instance.sentence = tokens[1];
-
-						// <sentence-level-features>
-						line = br.readLine();
-						if (!line.equals("<sentence-level-features>"))
-							throw new NotExpectedStringException(
-									"Expects <sentence-level-features> but getting "
-											+ line);
-						// sentence-level conjunctive features
-						featIndices = new ArrayList<Integer>();
-						while (!(line = br.readLine())
-								.equals("bag-of-words-features:")) {
-							index = sLvConjIndex.indexOf(line);
-							featIndices.add(index);
-						}
-						instance.sLvConjFeats = intList2Array(featIndices);
-
-						// bow-word-feature
-						// skips name
-						br.readLine();
-						instance.sLvWords = br.readLine().split(" ");
-
-						// bow-tag-feature
-						// skips name
-						br.readLine();
-						instance.sLvTags = strList2Array(br.readLine());
-
-						// bow-dep-feature
-						// skips name
-						br.readLine();
-						instance.sLvDeps = strList2Array(br.readLine());
-
-						// bow-dep-feature
-						// skips name
-						br.readLine();
-						instance.sLvDepWords = br.readLine().split(" ");
-
-						// </sentence-level-features>
-						// skips it
-						br.readLine();
-						
-						// chunk-level features
-						
-						
+						boolean breakPoint = true;
 					} catch (NotExpectedStringException e) {
 						System.out.println(e.errorMessage);
-						System.out.println("Looking for a new instance.");
+						// System.out.println("Looking for a new instance.");
 					}
-
 				}
 
 				// }
@@ -137,16 +78,187 @@ public class Wrapper {
 
 	}
 
+	static void readSentenceLevelFeatures(BufferedReader charStream,
+			Instance instance, HashIndex<String> featIndex) throws IOException,
+			NotExpectedStringException {
+		String line;
+		int index;
+		// <sentence-level-features>
+		line = charStream.readLine();
+		if (!line.equals("<sentence-level-features>"))
+			throw new NotExpectedStringException(
+					"Expects <sentence-level-features> but getting " + line
+							+ ". Looking for a new instance.");
+		// sentence-level conjunctive features
+		instance.sLvConjFeats = new int[instance.sLvFeatsCnt];
+		for (int i = 0; i < instance.sLvFeatsCnt; i++) {
+			line = charStream.readLine();
+			index = featIndex.indexOf(line, true);
+			instance.sLvConjFeats[i] = index;
+		}
+		// skips "bag-of-words-features"
+		charStream.readLine();
+		// skips "bow-word-feature"
+		charStream.readLine();
+		instance.sLvWords = charStream.readLine().split(" ");
+		// skips "bow-tag-feature"
+		charStream.readLine();
+		instance.sLvTags = strList2Array(charStream.readLine());
+		// skips "bow-dep-feature"
+		charStream.readLine();
+		instance.sLvDeps = strList2Array(charStream.readLine());
+		// skips "bow-dep-feature"
+		charStream.readLine();
+		instance.sLvDepWords = charStream.readLine().split(" ");
+		// skips "</sentence-level-features>"
+		charStream.readLine();
+	}
+
+	static Instance readBasicInfoAndInitializeInstance(BufferedReader charStream)
+			throws IOException {
+		Instance instance = new Instance();
+
+		String separator = ": ";
+		String[] tokens;
+		// index
+		tokens = charStream.readLine().split(separator);
+		instance.index = Integer.parseInt(tokens[1]);
+		// cui1
+		tokens = charStream.readLine().split(separator);
+		instance.cui1 = tokens[1];
+		// cui1-type
+		tokens = charStream.readLine().split(separator);
+		instance.cui1_type = tokens[1];
+		// cui1-matched_words
+		tokens = charStream.readLine().split(separator);
+		instance.cui1_matched_words = tokens[1];
+		// cui1-global-phrase-index
+		tokens = charStream.readLine().split(separator);
+		instance.cui1_index = Integer.parseInt(tokens[1]);
+		// cui2
+		tokens = charStream.readLine().split(separator);
+		instance.cui2 = tokens[1];
+		// cui2-type
+		tokens = charStream.readLine().split(separator);
+		instance.cui2_type = tokens[1];
+		// cui2-matched-words
+		tokens = charStream.readLine().split(separator);
+		instance.cui2_matched_words = tokens[1];
+		// cui2-global-phrase-index
+		tokens = charStream.readLine().split(separator);
+		instance.cui2_index = Integer.parseInt(tokens[1]);
+		// positivity
+		tokens = charStream.readLine().split(separator);
+		instance.positivity = Boolean.parseBoolean(tokens[1]);
+		// inverse
+		tokens = charStream.readLine().split(separator);
+		instance.inverse = Boolean.parseBoolean(tokens[1]);
+		// net-relation
+		tokens = charStream.readLine().split(separator);
+		instance.net_relation = tokens[1];
+		// meta-relation
+		tokens = charStream.readLine().split(separator);
+		instance.meta_relation = tokens[1];
+		// number-of-sentence-level-features
+		tokens = charStream.readLine().split(separator);
+		instance.sLvFeatsCnt = Integer.parseInt(tokens[1]);
+		// number-of-chunks
+		tokens = charStream.readLine().split(separator);
+		instance.chunksCnt = Integer.parseInt(tokens[1]);
+		// number-of-phrases
+		tokens = charStream.readLine().split(separator);
+		instance.phrasesCnt = Integer.parseInt(tokens[1]);
+		// number-of-words
+		tokens = charStream.readLine().split(separator);
+		instance.wordsCnt = Integer.parseInt(tokens[1]);
+		// sentence
+		tokens = charStream.readLine().split(separator);
+		instance.sentence = tokens[1];
+
+		instance.cLvConjFeats = new int[instance.chunksCnt * 2];
+		instance.cLvBOWFeats = new int[instance.chunksCnt * 2][];
+
+		instance.pLvConjFeats = new int[instance.phrasesCnt * 2];
+		instance.pLvBOWFeats = new int[instance.phrasesCnt * 2][];
+
+		instance.wLvConjFeats = new int[instance.wordsCnt * 2];
+		instance.wLvBOWFeats = new int[instance.wordsCnt * 2][];
+
+		return instance;
+	}
+
+	static Pair<Integer, Integer> readFeatureBlock(BufferedReader charStream,
+			HashIndex<String> featIndex, String elementName, int length,
+			Instance instance, int[] conjFeats, int[][] bowFeats)
+			throws NotExpectedStringException {
+		Pair<Integer, Integer> pair = new Pair<Integer, Integer>();
+		String line, substring;
+		String[] tokens;
+		String ne1Type = instance.cui1_type, ne2Type = instance.cui2_type;
+		int index;
+		ArrayList<String> substrings = new ArrayList<String>();
+
+		try {
+			line = charStream.readLine();
+			// elementName is the first line
+			if (!line.equals(elementName))
+				throw new NotExpectedStringException("Expects " + elementName
+						+ " but getting " + line
+						+ ". Looking for a new instance.");
+
+			// skips "word-features:"
+			charStream.readLine();
+			for (int i = 0; i < length; i++) {
+				line = charStream.readLine();
+				index = featIndex.indexOf(line,true);
+				conjFeats[i] = index;
+				tokens = line.split("\\|");
+				substring = tokens[1];
+				substrings.add(substring);
+				// no bow feature yet
+			}
+
+			pair.first = substrings.indexOf(ne1Type);
+			pair.second = substrings.lastIndexOf(ne2Type);
+
+			// skips "tag-features: "
+			charStream.readLine();
+			for (int i = 0; i < length; i++) {
+				line = charStream.readLine();
+				index = featIndex.indexOf(line,true);
+				line = charStream.readLine();
+				if (i != pair.first && i != pair.second) {
+					tokens = line.split(": ");
+					bowFeats[length + i] = strList2Array(tokens[1]);
+				}
+
+			}
+
+			// skips "</...>"
+			charStream.readLine();
+		} catch (IOException e) {
+			throw new NotExpectedStringException(
+					"IOExpection catched for feature block " + elementName);
+		}
+
+		return pair;
+	}
+
 	static class Instance {
-		int index, cui1_index, cui2_index;
+		int index, cui1_index, cui2_index, sLvFeatsCnt, chunksCnt, phrasesCnt,
+				wordsCnt;
 		String cui1, cui1_type, cui1_matched_words, cui2, cui2_type,
 				cui2_matched_words, net_relation, meta_relation, sentence;
 		boolean inverse, positivity;
-		int[] sLvConjFeats;
+		int[] sLvConjFeats, cLvConjFeats, pLvConjFeats, wLvConjFeats;
+		int[][] cLvBOWFeats, pLvBOWFeats, wLvBOWFeats;
+		int cLvNEIndex1, cLvNEIndex2, pLvNEIndex1, pLvNEIndex2, wLvNEIndex1,
+				wLvNEIndex2;
 		String[] sLvWords, sLvDepWords;
 		int[] sLvTags, sLvDeps;
 	}
 
+	// used for all exceptions in this class
 	static class NotExpectedStringException extends Exception {
 		String errorMessage;
 
